23df9a7d4f69992fb0b8d430e8f16bad68f2877d
SiriBasuthkar <basuthkarsiri.2004@gmail.com>
2025-08-12 20:37:38 +0530

    last commit

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -334,7 +334,6 @@ Educational Content Evaluation Platform
         <button className="flex items-center space-x-2 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-medium">
           {downloadReport}
           <Download className="h-5 w-5" />
-          <span></span>
         </button>
         <button 
           onClick={shareReport}
